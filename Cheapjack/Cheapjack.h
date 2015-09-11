46dbb9cbdcd1680554e95d5cc1975a5008cc7c58
Gurpartap Singh <contact@gurpartap.com>
2015-09-11 07:13:32 +0530

    cheapjack |ˈtʃiːpdʒak| noun
    "a seller of cheap inferior goods, typically a hawker at a fair or
    market." — Apple’s Dictionary v2.2.1

---
+++ Cheapjack/Cheapjack.h
@@ -0,0 +1,19 @@
+//
+//  Cheapjack.h
+//  Cheapjack
+//
+//  Created by Gurpartap Singh on 11/09/15.
+//  Copyright © 2015 Gurpartap Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cheapjack.
+FOUNDATION_EXPORT double CheapjackVersionNumber;
+
+//! Project version string for Cheapjack.
+FOUNDATION_EXPORT const unsigned char CheapjackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cheapjack/PublicHeader.h>
+
+
