1d67ad02e0bcbcd0a57c11e21edd57a753ec2093
Gurpartap Singh <contact@gurpartap.com>
2015-09-11 07:19:49 +0530

    CheapjackExample project

---
+++ CheapjackExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheapjackExample
+//
+//  Created by Gurpartap Singh on 11/09/15.
+//  Copyright © 2015 Gurpartap Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
